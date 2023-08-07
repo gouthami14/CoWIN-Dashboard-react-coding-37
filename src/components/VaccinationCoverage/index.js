8911b9620e066b48fc037e01871c056731318319
gouthami14 <92106741+gouthami14@users.noreply.github.com>
2023-08-07 18:28:01 +0530

    Update index.js

--- src/components/VaccinationCoverage/index.js
+++ src/components/VaccinationCoverage/index.js
@@ -27,7 +27,7 @@ const VaccinationCoverage = props => {
           dataKey="vaccineData"
           tick={{
             stroke: 'gray',
-            strokeWidth: '1',
+            strokeWidth: 1,
             fontSize: 15,
             fontFamily: 'Roboto',
           }}
@@ -36,21 +36,21 @@ const VaccinationCoverage = props => {
           tickFormatter={dataFormatter}
           tick={{
             stroke: 'gray',
-            strokeWidth: '0.5',
+            strokeWidth: 0.5,
             fontSize: 15,
             fontFamily: 'Roboto',
           }}
         />
         <Legend
           wrapperStyle={{
-            paddingTop: 30,
+            paddingTop: 20,
             textAlign: 'center',
             fontSize: 12,
             fontFamily: 'Roboto',
           }}
         />
-        <Bar dataKey="does_1" name="Dose 1" barSize="20%" fill="#5a8dee" />
-        <Bar dataKey="does_2" name="Dose 2" barSize="20%" fill=" #f54394" />
+        <Bar dataKey="does1" name="Dose 1" barSize="20%" fill="#5a8dee" radius={{10, 10, 0, 0}} />
+        <Bar dataKey="does2" name="Dose 2" barSize="20%" fill=" #f54394" radius={{5, 5, 0, 0}} />
       </BarChart>
     </div>
   )
